86997d62011bde5a1df4aa8b0180da986a9ffe79
Hasan Can ÖZDEMİR <153028563+hasancanozdemir@users.noreply.github.com>
2025-12-18 14:49:43 +0300

    Delete parasut_muhasebe_entegrasyonu directory

--- parasut_muhasebe_entegrasyonu/models/account_journal.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-class AccountJournal(models.Model):
-    _inherit = 'account.journal'
-
-    parasut_id = fields.Char(string='Parasut Account ID', help="Unique ID from Parasut for Banks/Cash", copy=False, index=True)
